2d2a4e5ef918d78479c51eedc1f1f69b85bd4a43
AgusBlanco <agustinanahirblanco@gmail.com>
2024-08-27 19:33:55 -0300

    Primeros pasos en la especialización-Backend

---
+++ src/Products/products.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class ProductsService {
+
+    getProductsService() {
+        return "Hola desde el servicio de Productos"
+    }
+}
